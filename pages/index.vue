6066fdc70b04145c1d7d01d6d4ab1585b2fe58bb
Nikola <n.micunovic@net2.one>
2022-01-06 08:44:23 +0100

    gitpages

---
+++ pages/index.vue
@@ -0,0 +1,390 @@
+<template>
+   
+    <div>
+      <SocialHead
+      :title="'Hausmajstor Beograd'"
+      :description="'Kucni majstor na teritoriji Beograda. Sitne popravke po kuci. Dolazimo po pozivu'"
+      :image="'/og-image.png'"
+    />
+         <b-navbar toggleable="lg" type="dark" variant="info">
+             <b-navbar-brand href="#" >
+      <img src="/logo.png">
+    </b-navbar-brand>
+<b-collapse id="nav-collapse" is-nav>
+    <b-button variant="outline-info" class="mb-2">
+      <b-icon icon="truck" aria-hidden="true"></b-icon> Stižemo brzo do svih naselja u Beogradu
+    </b-button>
+    <b-button variant="outline-info" class="mb-2">
+      <b-icon icon="alarm" aria-hidden="true"></b-icon> Trudimo se da budemo tačni, jer znamo koliko vreme vredi
+    </b-button>
+
+    
+
+    <b-button variant="outline-info" class="mb-2">
+      <b-icon icon="hammer" aria-hidden="true"></b-icon> Opremljeni smo najkvalitetnijim alatom
+    </b-button>
+    <b-button variant="outline-info" class="mb-2">
+      <b-icon icon="emoji-smile" aria-hidden="true"></b-icon> Naša najbolja reklama su zadovoljni klijenti
+    </b-button>
+</b-collapse>
+         </b-navbar>
+   
+        
+        <div>
+
+
+            <div>
+                <div class="interior-header">
+                    <b-img class="imgslider" fluid src="/first-img.png">
+                    </b-img>
+
+                    <h1 class="header-tekst">Potreban ti je hausmajstor na teritoriji Beograda?</h1>
+                
+                  
+                </div>
+            </div>
+
+           
+
+        </div>
+
+        <b-container class=" align-self-center" style="background-color:#6B5FCF; max-width:100%">
+            <b-container>
+            <b-row>
+                <b-col cols="12" sm="12" lg="6" class="align-self-center">
+                    <h2 class="prikaz-naslov2">Montaža na zid</h2>
+                    <p class="prikaz-txt2">Kupili ste policu koju sada treba montirati na zid? Ili želite okačiti ram sa slikom? Bez brige! Izbušićemo bezbedno zid, izabrati odgovarajuću tiplu i montirati šta god želite uz hiruršku preciznost ;) !</p>
+                </b-col>
+                <b-col cols="12" sm="12" lg="6" class="align-self-center">
+                    <b-img class="img-prikaz2" center fluid src="/montaza.png" />
+                </b-col>
+
+            </b-row>
+            </b-container>
+        </b-container>
+
+        <b-container class="align-self-center" >
+            <b-container>
+            <b-row>
+                <b-col class="align-self-center" cols="12" sm="12" lg="6" order-sm="12" order="12" order-md="1" order-lg="1" order-xl="1" >
+                    <b-img class="img-prikaz" center fluid src="/zacepljenje.png" />
+                </b-col>
+                <b-col class="align-self-center" cols="12" sm="12" lg="6" order-sm="1" order="1" order-md="12" order-lg="12" order-xl="12">
+
+                    <h2 class="prikaz-naslov">Sudopera se začepila?</h2>
+                    <p class="prikaz-txt">Znamo odlično koliko to može biti frustrirajuće. Ali naše iskustvo u kombinaciji sa odličnim alatom otkloniće problem očas posla! Usput, otkrićemo Vam tajnu kako da sprečite česta začepljenja ;)</p>
+                </b-col>
+
+            </b-row>
+            </b-container>
+        </b-container>
+
+     
+ <b-container class="align-self-center" style="background-color:white; padding:15px; max-width:95%; color:#BF1B2C; font-weight:800" >
+            <b-row class="justify-content-center">
+            <p  style="font-size: calc(1rem + 0.8vw); text-align:center; line-height: normal;">S nama možete zaboraviti na onu čuvenu: "Drži vodu dok majstori odu!"</p>
+            </b-row>
+        </b-container>
+
+
+ <b-container class=" align-self-center" style="background-color:#6B5FCF; max-width:100%">
+            <b-container>
+            <b-row>
+                <b-col cols="12" sm="12" lg="6" class="align-self-center">
+                    <h2 class="prikaz-naslov2">Rasklimani prozori, roletne, vrata...</h2>
+                    <p class="prikaz-txt2">Najčešće tek kada dođe zima shvatimo koliko je važno da su nam prozori ispravni. Dobro zaptivanje sprečiće prodiranje vode i hladnoće. Primetili ste da su vrata "pala", škripe prilikom zatvaranja? Nikakav problem, za sve imamo rešenje :)</p>
+                </b-col>
+                <b-col cols="12" sm="12" lg="6" class="align-self-center">
+                    <b-img class="img-prikaz2" center fluid src="/prozor.png" />
+                </b-col>
+
+            </b-row>
+            </b-container>
+        </b-container>
+
+<b-container class="align-self-center" >
+            <b-container>
+            <b-row>
+                <b-col class="align-self-center" cols="12" sm="12" lg="6" order-sm="12" order="12" order-md="1" order-lg="1" order-xl="1" >
+                    <b-img class="img-prikaz" center fluid src="/tablet.png" />
+                </b-col>
+                <b-col class="align-self-center" cols="12" sm="12" lg="6" order-sm="1" order="1" order-md="12" order-lg="12" order-xl="12">
+
+                    <h2 class="prikaz-naslov">U korak s tehnologijom!</h2>
+                    <p class="prikaz-txt">Nove tehnologije zahtevaju nova znanja. Trudimo se da budemo u toku sa najnovijim uređajima i alatima kako bismo rešili sve probleme naših klijenata.</p>
+                </b-col>
+
+            </b-row>
+            </b-container>
+        </b-container>
+
+<b-container class=" align-self-center" style="background-color:#6B5FCF; max-width:100%">
+            <b-container>
+            <b-row>
+                <b-col cols="12" sm="12" lg="6" class="align-self-center">
+                    <h2 class="prikaz-naslov2">Muči Vas elektrika po kući?</h2>
+                    <p class="prikaz-txt2">Pita nije jednako pečene u rerni? Zamena grejača rerne, sijaličnih grla, povezivanje novog štednjaka, zamena osigurača samo su neki od problema koje možemo rešiti.</p>
+                </b-col>
+                <b-col cols="12" sm="12" lg="6" class="align-self-center">
+                    <b-img class="img-prikaz2" center fluid src="/elektricar.png" />
+                </b-col>
+
+            </b-row>
+            </b-container>
+        </b-container>
+
+        <b-container class=" align-self-center" style="background-color:#6B5FCF; max-width:100%">
+            <b-container>
+            <b-row >
+                <b-col cols="12" sm="12" lg="6" class="align-self-center">
+                    <h2 class="prikaz-naslov2">Šta još radimo?</h2>
+                    <p class="prikaz-txt2">Iskreno, mogli bismo ovako nabrajati u nedogled. Kroz godine rada nailazili smo na različite zahteve i probleme i pronalazili još različitija rešenja. Jasno nam je da ne možemo sve pobrojati, zato predlažemo nešto mnogo jednostavnije: Pozovite nas i lako ćemo se dogovoriti oko svega!</p>
+                </b-col>
+                <b-col cols="12" sm="12" lg="6" class=" align-self-center prikaz-naslov2">
+                    <b-button class="dugme-donje" pill variant="primary" href="tel:+381643092904">
+                  <b-icon icon="telephone" class="rounded-circle bg-danger p-2" scale="1" variant="light"></b-icon> 0643092904
+                    </b-button>
+                </b-col>
+
+            </b-row>
+            </b-container>
+        </b-container>
+
+        
+
+
+      
+
+        
+
+    </div>
+
+
+    
+</template>
+
+<script>
+    export default {
+     
+        name: 'indexPage',
+        components: {
+           
+        }
+    };
+</script>
+
+<style scoped>
+.navbar {
+    justify-content: center;
+}
+.header-stl {
+    min-height: 80px;
+    width: 100%;
+    margin: 0;
+
+    background-color: coral;
+}
+    .nav-link{
+        color:white;
+    }
+    .sidebarcol {
+        background-color: #0aa8a8
+    }
+    .menutext {
+        color:azure;
+        outline-color:white;
+        border-bottom-color:white;
+        border-bottom-width:medium;
+    }
+    .navbar-custom{
+        background-color:white !important;
+    }
+    .navbar.navbar-dark.bg-info.navbar-expand-lg {
+        background-color:#0c7e86 !important;
+    }
+    .btn.mb-2.btn-outline-info {
+        border-color:white;
+        margin:10px 30px 10px 30px;
+        color:white;
+        width: stretch;
+    }
+    p {
+        line-height: 1rem;
+    }
+    .card {
+        padding: 20px;
+    }
+    
+    .fade-enter-active,
+    .fade-leave-active {
+        transition: opacity .5s;
+    }
+    .fade-enter,
+    .fade-leave-to {
+        opacity: 0;
+    }
+    
+    h1 {
+        margin-bottom: 1.5rem;
+    }
+    
+    
+    
+    .exrbtn {
+        font-size:10px;
+        margin:3px !important
+        
+    }
+    .home-slider {
+        margin:0px;
+    }
+    .imgslider {
+        min-width:100%;
+    }
+    .header-tekst {
+        position: absolute;
+        top: 20%;
+        left: 40%;
+        transform: translate(-60%, -25%);
+        -ms-transform: translate(-45%, -50%);
+        color: black;
+        font-size:5vw;
+        font-weight:900;
+        font-family:  'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif
+        
+       
+    }
+    .btn-android {
+        position: absolute;
+        top: 40%;
+        left: 30%;
+        transform: translate(-60%, -25%);
+        -ms-transform: translate(-45%, -50%);
+        color: white;
+        font-size:2.7vw;
+        font-weight: bold;
+        background-color: rgb(19, 92, 175);
+    }
+    .btn-ios {
+        position: absolute;
+        top: 60%;
+        left: 50%;
+        transform: translate(-60%, -25%);
+        -ms-transform: translate(-45%, -50%);
+        color: white;
+        font-size: 3vw;
+        font-weight: bold;
+    }
+    .interior-header {
+        display: block;
+        position: relative;
+        width: 100%;
+        height: auto;
+        border: 1px solid #b22222;
+        padding: 1px;
+    }
+    .img-prikaz {
+        margin:15px;
+        padding:5px;
+        border-radius: 10%;
+        
+         
+    }
+
+    .img-prikaz2 {
+        margin:25px;
+        padding:5px;
+        border-radius: 10%;
+         
+    }
+
+.prikaz-txt2 {
+         font-size: calc(0.8rem + 0.8vw);
+        line-height:normal;
+        padding: 5px 20px 5px 10px;
+        position:center;
+        color:white;
+        text-align: center;
+        
+    }
+    .prikaz-naslov2 {
+        font-size: calc(1.375rem + 1.5vw);
+        line-height: normal;
+        padding: 15px 10px 5px 10px;
+        position: center;
+        font-weight:800;
+        color:white;
+        text-align: center;
+        
+    }
+
+    .prikaz-txt {
+        font-size: calc(0.8rem + 0.8vw);
+        line-height:normal;
+        padding: 5px 20px 5px 10px;
+        position:center;
+        text-align: center;
+        
+    }
+    .prikaz-naslov {
+        font-size: calc(1.375rem + 1.5vw);
+        line-height: normal;
+        padding: 15px 10px 5px 10px;
+        font-weight:800;
+        position: center;
+        
+        text-align: center;
+        
+    }
+    .prikaz-kontejner{
+        margin:20px 10px 10px 10px;
+    }
+    .banner-txt {
+        position: absolute;
+        top: 50%;
+        left: 18%;
+        transform: translate(-20%, -25%);
+        -ms-transform: translate(-20%, -30%);
+        color: white;
+        font-size: 3.5vw;
+        font-weight: bold;
+        margin: 5px;
+        
+    }
+    .prikaz-txt-chat {
+        font-size: 2vw;
+        line-height: normal;
+        padding: 5px 30px 5px 40px;
+        position: center
+    }
+    .prikaz-naslov-chat {
+        font-size: 3vw;
+        line-height: normal;
+        padding: 15px 30px 5px 40px;
+        position: center;
+        font-weight: bold;
+    }
+    .footer-cont {
+        background-color:white;
+    }
+    .footer-txt{
+        color:black;
+        justify-content:right;
+        text-align:right;
+    }
+    
+    .privacy-style{
+        line-height:normal;
+    }
+
+    .dugme-donje {
+        font-size:calc(1.375rem + 1.5vw);
+        background-color:rgb(69, 176, 238);
+        font-weight: 800;
+        border: 3px solid white;
+    }
+
+
+
+
+</style>
